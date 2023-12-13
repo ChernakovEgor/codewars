0a9742029888e2e785f8edf00c1f57010afcb0b9
Egor Chernakov <chernakov.eg@gmail.com>
2023-12-13 12:28:58 +0300

    8k

--- c++/8kyu/yes_no.cpp
+++ c++/8kyu/yes_no.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
 #include <string>
 
-std::string bool_to_word(bool value) {
-  if (value)
-    return "Yes";
-  else
-    return "No";
-}
+std::string bool_to_word(bool value) { return value ? "Yes" : "No"; }
 
 int main() {
   std::cout << bool_to_word(false);
